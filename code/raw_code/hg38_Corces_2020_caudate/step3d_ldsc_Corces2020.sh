6f5339912c5e67b8fd4b89b372386d0abecf39dd
badoi <badoi@me.com>
2021-01-27 21:02:52 -0500

    ldsc heritability files for conditional analyses

--- code/raw_code/hg38_Corces_2020_caudate/step3d_ldsc_Corces2020.sh
+++ code/raw_code/hg38_Corces_2020_caudate/step3d_ldsc_Corces2020.sh
@@ -1,46 +1,89 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH --partition=pool1
-#SBATCH --time 0-8
-#SBATCH --job-name=enrich_Corces2020
+#SBATCH --time 1-0
+#SBATCH --job-name=est_herit
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
-#SBATCH --error=logs/run_enrich_Corces2020_%A_%a.txt
-#SBATCH --output=logs/run_enrich_Corces2020_%A_%a.txt
-#SBATCH --array=1-62
+#SBATCH --mem=8G
+#SBATCH --error=logs/estim_heritability_%A_%a.txt
+#SBATCH --output=logs/estim_heritability_%A_%a.txt
+#SBATCH --array=1-68
 
-# get the GWAS for this array job
-GWASDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments
+log2results() {
+	awk -F '\t' '/Total Observed scale h2*/{flag=1;next}/Lambda GC/{flag=0}flag' ${OUT}.log | \
+	# append line with leading white space to previous
+	sed ':a;N;$!ba;s/\n //g'| \
+	# parse Partitioned heritability rownames, starts w/ colon
+	awk -F":" -v OFS='\t' '{gsub("[[:space:]]", "_", $1); print}' | \
+	# transpose row to columns
+	awk -v OFS='\t' '
+	{ gsub(":", "")
+	    for (i=1; i<=NF; i++)  {
+	    a[NR,i] = $i
+	}
+	}
+	NF>p { p = NF }
+	END {    
+	for(j=1; j<=p; j++) {	for(i=2; i<=NR; i++){
+	str=str"\t"a[i,j];
+	}
+	print str
+	}
+	}' | \
+	awk -v OFS='\t' '{gsub("_[0-9]+$", "", $1); gsub("L2$", "", $1); print}' | \
+	# Add in Conditional Regression Tau Coefficient Z-score
+	awk -v OFS='\t'  '{s=(NR==1)?"Coefficient_z":$7/$8;$0=$0 OFS s}1' | \
+	awk -v OFS='\t'  '{print $1, $2, $3, $9, $4, $5, $6, $7, $8, $10 }' | \
+	gzip > ${OUT}.results.gz
+}
+
+## set up the different directors of where files are
 SETDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/snATAC_cross_species_caudate
-DATADIR=${SETDIR}/data/raw_data/caudate_conservation_ldsc
-cd $SETDIR; source activate ldsc
+GWASDIR=${SETDIR}/data/tidy_data/ldsc_gwas; SNPLIST=${GWASDIR}/listHM3.noMHC.txt
+CODEDIR=${SETDIR}/code/raw_code/hg38_Corces_2020_caudate; DATADIR=${SETDIR}/data/raw_data/caudate_conservation_ldsc
+cd $CODEDIR
+source activate ldsc; mkdir -p ${DATADIR}/enrichments
 
-# get the GWAS and reference population
-GWAS=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND + 1) {print $1}' ${DATADIR}/gwas_list_sumstats.tsv)
-POP=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND + 1) {print $2}' ${DATADIR}/gwas_list_sumstats.tsv)
-GWAS_Label=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND+ 1) {print $3}' ${DATADIR}/gwas_list_sumstats.tsv)
+## get the GWAS and reference population
+GWAS=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND + 1) {print $1}' ${GWASDIR}/gwas_list_sumstats.tsv)
+POP=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND + 1) {print $2}' ${GWASDIR}/gwas_list_sumstats.tsv)
+GWAS_Label=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND+ 1) {print $3}' ${GWASDIR}/gwas_list_sumstats.tsv)
+PEAKTYPES=$(stat -c "%n" $DATADIR/annotation/*.1.annot.gz | sed 's!.*/!!' | cut -d'.' -f1 | sort | uniq )
 
-OUTDIR=${DATADIR}/enrichments; mkdir -p $OUTDIR
-
-###################################################################
-# run LD score regression over the Corces caudate binary annotations
-CTS_FN1=${DATADIR}/Corces2020_binary_${POP}_hg38_celltypes.ldcts
-if [[ ! -f $OUTDIR/Corces2020_binary.${GWAS_Label}.${POP}.cell_type_results.txt ]]; then
-ldsc.py --ref-ld-chr-cts $CTS_FN1 \
---ref-ld-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/baseline_v1.1/baseline_v1.1.${POP}. \
+########################################################################
+## Estimate SNP heritability on binary Meuleman DHS index annotations ##
+for PEAKTYPE in $PEAKTYPES; do
+## use DHS/roadmap/cCRE/Corces2020 merged peaks as bg instead of All
+BINARY_DHS_BG=${SETDIR}/data/raw_data/caudate_conservation_ldsc/annotation/Corces2020_caudate.All_Roadmap_DHS_cCREs.BG.${POP}.,
+CELLTYPES=$(stat -c "%n" $DATADIR/annotation/${PEAKTYPE}.*.${POP}.1.annot.gz | sed "s/${POP}.*/${POP}.,/g" | sed '/Consensus/d' | sed '/All/d' | sort| uniq | sed -z 's/\n//g')
+OUT=${DATADIR}/enrichments/${PEAKTYPE}.binary.${GWAS_Label}.${POP}
+if [[ ! -f "${OUT}.results.gz" ]]; then
+ldsc.py --h2 $GWAS --print-coefficients --print-snps ${SNPLIST} --n-blocks 400 \
 --w-ld-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/weights/1000G.${POP}.weights.hm3_noMHC. \
---h2-cts $GWAS --out $OUTDIR/Corces2020_binary.${GWAS_Label}.${POP}
+--ref-ld-chr ${CELLTYPES}${BINARY_DHS_BG}${GWASDIR}/1000G_ALL_Phase3_hg38_files/baselineLD_v2.2/baselineLD_v2.2.${POP}. \
+--frqfile-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/plink/1000G.${POP}.HM3. \
+--out ${OUT} 
+log2results
 fi
+## format logs file to make .results file, --print-snps doesn't make a .results file
 
-#################################################################################
-# run LD score regression over the Corces caudate peaks intersect Zoonomia phyloP
-CTS_FN2=${DATADIR}/Corces2020_phyloP_${POP}_hg38_celltypes.ldcts
-if [[ ! -f $OUTDIR/Corces2020_phyloP.${GWAS_Label}.${POP}.cell_type_results.txt ]]; then
-ldsc.py --ref-ld-chr-cts $CTS_FN2 \
---ref-ld-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/baseline_v1.1/baseline_v1.1.${POP}. \
+#############################################################################################
+## Estimate SNP heritability on continuous phyloP scores intersected w/ Meuleman DHS peaks ##
+CONTIN_DHS_BG=${SETDIR}/data/raw_data/caudate_conservation_ldsc/annot_phyloP/Corces2020_caudate.All_Roadmap_DHS_cCREs.BG.${POP}.,
+CELLTYPES=$(stat -c "%n" $DATADIR/annot_phyloP/${PEAKTYPE}.*.${POP}.1.annot.gz | sed "s/${POP}.*/${POP}.,/g" | sed '/Consensus/d'| sed '/All/d' | sort| uniq | sed -z 's/\n//g')
+OUT=${DATADIR}/enrichments/${PEAKTYPE}.phyloP.${GWAS_Label}.${POP}
+if [[ ! -f "${OUT}.results.gz" ]]; then
+ldsc.py --h2 $GWAS --print-coefficients --print-snps ${SNPLIST} \
 --w-ld-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/weights/1000G.${POP}.weights.hm3_noMHC. \
---h2-cts $GWAS --out $OUTDIR/Corces2020_phyloP.${GWAS_Label}.${POP}
+--ref-ld-chr ${CELLTYPES}${CONTIN_DHS_BG}${GWASDIR}/1000G_ALL_Phase3_hg38_files/baselineLD_v2.2/baselineLD_v2.2.${POP}. \
+--frqfile-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/plink/1000G.${POP}.HM3. \
+--out ${OUT} 
+log2results 
 fi
+## format logs file to make .results file, --print-snps doesn't make a .results file
+done
+
+
 
 
--- code/raw_code/meuleman_dhs_index/step3a_estim_heritability_meuleman_dhs_index.sh
+++ code/raw_code/meuleman_dhs_index/step3a_estim_heritability_meuleman_dhs_index.sh
@@ -6,14 +6,12 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
-#SBATCH --error=logs/estim_heritability_%A_%a.err.txt
-#SBATCH --output=logs/estim_heritability_%A_%a.out.txt
-#SBATCH --array=1-65
+#SBATCH --error=logs/estim_heritability_%A_%a.txt
+#SBATCH --output=logs/estim_heritability_%A_%a.txt
+#SBATCH --array=1-68
 
 log2results() {
-	IN=$1
-	OUT=$(echo $IN| sed 's/.log$/.results.gz/g')
-	awk -F '\t' '/Total Observed scale h2*/{flag=1;next}/Lambda GC/{flag=0}flag' $IN | \
+	awk -F '\t' '/Total Observed scale h2*/{flag=1;next}/Lambda GC/{flag=0}flag' ${OUT}.log | \
 	# append line with leading white space to previous
 	sed ':a;N;$!ba;s/\n //g'| \
 	# parse Partitioned heritability rownames, starts w/ colon
@@ -27,55 +25,69 @@ log2results() {
 	}
 	NF>p { p = NF }
 	END {    
-	for(j=1; j<=p; j++) {
-	str=a[1,j]
-	for(i=2; i<=NR; i++){
+	for(j=1; j<=p; j++) {	for(i=2; i<=NR; i++){
 	str=str"\t"a[i,j];
 	}
 	print str
 	}
 	}' | \
 	awk -v OFS='\t' '{gsub("_[0-9]+$", "", $1); gsub("L2$", "", $1); print}' | \
-	# Add in Observed Scale heritability Z-score
-	awk -v OFS='\t'  '{s=(NR==1)?"Observed_scale_z":$2/$3;$0=$0 OFS s}1' | \
 	# Add in Conditional Regression Tau Coefficient Z-score
 	awk -v OFS='\t'  '{s=(NR==1)?"Coefficient_z":$7/$8;$0=$0 OFS s}1' | \
 	awk -v OFS='\t'  '{print $1, $2, $3, $9, $4, $5, $6, $7, $8, $10 }' | \
-	gzip > ${OUT}
+	gzip > ${OUT}.results.gz
 }
 
-# set up the different directors of where files are
-GWASDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments
-SNPLIST=${GWASDIR}/1000G_EUR_Phase3_GRCh38_files/hapmap3_snps/listHM3.noMHC.txt
+## set up the different directors of where files are
 SETDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/snATAC_cross_species_caudate
+GWASDIR=${SETDIR}/data/tidy_data/ldsc_gwas; SNPLIST=${GWASDIR}/listHM3.noMHC.txt
 CODEDIR=${SETDIR}/code/raw_code/meuleman_dhs_index; DATADIR=${SETDIR}/data/raw_data/meuleman_dhs_index
 cd $CODEDIR
 source activate ldsc; mkdir -p ${DATADIR}/enrichments
 
-# get the GWAS and reference population
+## get the GWAS and reference population
 GWAS=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND + 1) {print $1}' ${GWASDIR}/gwas_list_sumstats.tsv)
 POP=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND + 1) {print $2}' ${GWASDIR}/gwas_list_sumstats.tsv)
 GWAS_Label=$(awk -F '\t' -v IND=${SLURM_ARRAY_TASK_ID} 'NR==(IND+ 1) {print $3}' ${GWASDIR}/gwas_list_sumstats.tsv)
 PEAKTYPES=$(stat -c "%n" $DATADIR/peaks/*.narrowPeak.gz | sed 's!.*/!!' | cut -d'.' -f1 | sort | uniq )
 
+#######################################################################
+# Estimate SNP heritability on binary Meuleman DHS index annotations ##
+# use DHS/roadmap/cCRE/Corces2020 merged peaks as bg instead of All
+BINARY_DHS_BG=${SETDIR}/data/raw_data/caudate_conservation_ldsc/annotation/Corces2020_caudate.All_Roadmap_DHS_cCREs.BG.${POP}.,
 for PEAKTYPE in $PEAKTYPES; do
-	## run LD score regression to get estimate SNP heritability for the full-length peaks, remove All b/c collinearity
-	CELLTYPES=$(stat -c "%n" $DATADIR/annotation/${PEAKTYPE}.*.full.*${POP}.1.annot.gz | sed "s/${POP}.*/${POP}.,/g"| sed '/All/d' | sort| uniq | sed -z 's/\n//g')
-	ldsc.py --h2 $GWAS --print-coefficients --print-snps ${SNPLIST} --n-blocks 1000 \
-	--w-ld-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/weights/1000G.${POP}.weights.hm3_noMHC. \
-	--ref-ld-chr ${CELLTYPES}${GWASDIR}/1000G_ALL_Phase3_hg38_files/baselineLD_v2.2/baselineLD_v2.2.${POP}. \
-	--frqfile-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/plink/1000G.${POP}.HM3. \
-	--out ${DATADIR}/enrichments/${PEAKTYPE}.full.${GWAS_Label}.${POP}
-	## format logs file to make .results file, --print-snps doesn't make a .results file
-	log2results ${DATADIR}/enrichments/${PEAKTYPE}.binary.full.${GWAS_Label}.${POP}.log
+	for PEAKLENGTH in full core; do
+		CELLTYPES=$(stat -c "%n" $DATADIR/annotation/${PEAKTYPE}.*.${PEAKLENGTH}.*${POP}.1.annot.gz | sed "s/${POP}.*/${POP}.,/g" | sed '/All/d' | sed '/stromal_b/d' | sort| uniq | sed -z 's/\n//g')
+		OUT=${DATADIR}/enrichments/${PEAKTYPE}.binary.${PEAKLENGTH}.${GWAS_Label}.${POP}
+		if [[ ! -f ${OUT}.results.gz ]]; then
+			## Full DHS Index Peaks
+			ldsc.py --h2 $GWAS --print-coefficients --print-snps ${SNPLIST}	\
+			--w-ld-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/weights/1000G.${POP}.weights.hm3_noMHC. \
+			--ref-ld-chr ${CELLTYPES}${BINARY_DHS_BG}${GWASDIR}/1000G_ALL_Phase3_hg38_files/baselineLD_v2.2/baselineLD_v2.2.${POP}. \
+			--frqfile-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/plink/1000G.${POP}.HM3. \
+			--out ${OUT}
+			## format logs file to make .results file, --print-snps doesn't make a .results file
+			log2results 
+		fi
+	done
+done
 
-	## run LD score regression to get estimate SNP heritability for the core peaks, remove All b/c collinearity
-	CELLTYPES=$(stat -c "%n" $DATADIR/annotation/${PEAKTYPE}.*.core.*${POP}.1.annot.gz | sed "s/${POP}.*/${POP}.,/g"| sed '/All/d' | sort| uniq | sed -z 's/\n//g')
-	ldsc.py --h2 $GWAS --print-coefficients --print-snps ${SNPLIST} --n-blocks 1000 \
-	--w-ld-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/weights/1000G.${POP}.weights.hm3_noMHC. \
-	--ref-ld-chr ${CELLTYPES}${GWASDIR}/1000G_ALL_Phase3_hg38_files/baselineLD_v2.2/baselineLD_v2.2.${POP}. \
-	--frqfile-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/plink/1000G.${POP}.HM3. \
-	--out ${DATADIR}/enrichments/${PEAKTYPE}.core.${GWAS_Label}.${POP}
-	## format logs file to make .results file, --print-snps doesn't make a .results file
-	log2results ${DATADIR}/enrichments/${PEAKTYPE}.binary.core.${GWAS_Label}.${POP}.log
+#############################################################################################
+## Estimate SNP heritability on continuous phyloP scores intersected w/ Meuleman DHS peaks ##
+CONTIN_DHS_BG=${SETDIR}/data/raw_data/caudate_conservation_ldsc/annot_phyloP/Corces2020_caudate.All_Roadmap_DHS_cCREs.BG.${POP}.,
+for PEAKTYPE in $PEAKTYPES; do
+	for PEAKLENGTH in full core; do
+		## Full DHS Index Peaks###
+		CELLTYPES=$(stat -c "%n" $DATADIR/annot_phyloP/${PEAKTYPE}.*.${PEAKLENGTH}.*${POP}.1.annot.gz | sed "s/${POP}.*/${POP}.,/g" | sed '/All/d' | sed '/stromal_b/d' | sort| uniq | sed -z 's/\n//g')
+		OUT=${DATADIR}/enrichments/${PEAKTYPE}.phyloP.${PEAKLENGTH}.${GWAS_Label}.${POP}
+		if [[ ! -f ${OUT}.results.gz ]]; then
+			ldsc.py --h2 $GWAS --print-coefficients --print-snps ${SNPLIST}	\
+			--w-ld-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/weights/1000G.${POP}.weights.hm3_noMHC. \
+			--ref-ld-chr ${CELLTYPES}${CONTIN_DHS_BG}${GWASDIR}/1000G_ALL_Phase3_hg38_files/baselineLD_v2.2/baselineLD_v2.2.${POP}. \
+			--frqfile-chr ${GWASDIR}/1000G_ALL_Phase3_hg38_files/plink/1000G.${POP}.HM3. \
+			--out ${OUT}
+			## format logs file to make .results file, --print-snps doesn't make a .results file
+			log2results 
+		fi
+	done
 done

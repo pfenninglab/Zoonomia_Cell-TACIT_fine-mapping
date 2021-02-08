7ea41f79d39e37b301e2a6f21a83237ba108bf30
badoi <badoi@me.com>
2021-02-08 15:01:27 -0500

    recover UKB snps w/out db151 or db150 hg38 positions

--- code/raw_code/snp_annotations/step2_extract_ukb10k_snp_for_halper.py
+++ code/raw_code/snp_annotations/step2_extract_ukb10k_snp_for_halper.py
@@ -14,12 +14,16 @@ os.chdir(f'{SETDIR}/code/raw_code/snp_annotations')
 os.system(f'mkdir -p {BEDDIR} {OUTDIR} logs')
 # os.getcwd()
 
-SOURCE='Homo_sapiens'
-SLURM_CALL=f'sbatch --mem 4G -p pfen1 -w compute-1-39 {SETDIR}/code/raw_code/hal_scripts/halper_map_peak_orthologs.sh'
 zoo_df = pd.read_csv(f'{ZOONOMIADIR}/tables/200_Mammals_Genome_Information.tsv', sep = '\t')
-zoo_df = zoo_df.sort_values(by='Zoonomia Index')
+# zoo_df = zoo_df.sort_values(by='Zoonomia Index')
 zoo_df = zoo_df[zoo_df['Species'] != 'Homo_sapiens']
 zoo_df = zoo_df.sort_index(ascending=False)
+TARGETS=zoo_df['Species'].str.cat(sep=',')
+SOURCE='Homo_sapiens'
+SLURM_CALL=f'sbatch --mem 4G -p pfen1 -w compute-1-39 {SETDIR}/code/raw_code/hal_scripts/halper_map_peak_orthologs.sh'
+
+# prepare empty dataframe to stash SNPs w/o db150 or db151 snps
+notFound = pd.DataFrame()
 
 for CHR in range(1,23):
 	ANNOTFN=f"{BEDDIR}/UKB_imputed_snps.GRCh38.{CHR}.bed.gz"
@@ -37,6 +41,7 @@ for CHR in range(1,23):
 		df_dbsnp = pd.concat([df_b150, df_b151], axis=0).drop_duplicates(subset=['SNP', 'START','END'])
 		# get the hg38 bed coordinates of UKB snps, drop missing in UKB
 		df_out = pd.merge(df_annot, df_dbsnp, how = 'left', on='SNP')
+		notFound = notFound.append(df_out[~df_out['SEQ'].notna()])
 		df_out = df_out[df_out['SEQ'].notna()]
 		df_out.START = df_out.START.astype(int)
 		df_out.END = df_out.END.astype(int)
@@ -44,15 +49,33 @@ for CHR in range(1,23):
 		df_out.loc[indZeroWidth, ('END')] = df_out.loc[indZeroWidth, ('START')] + 1
 		np.sum(df_out.START ==df_out.END)
 		# read in the GRCh38 SNP coordinates, by rsID
-		df_out.to_csv(ANNOTFN, columns = ['SEQ','START','END','NAME'],
-		header = False, index=False, sep = '\t', compression="gzip")
+		# df_out.to_csv(ANNOTFN, columns = ['SEQ','START','END','NAME'],
+		# header = False, index=False, sep = '\t', compression="gzip")
 
-for CHR in range(1,11):
+for CHR in range(1,23):
 	ANNOTFN=f"{BEDDIR}/UKB_imputed_snps.GRCh38.{CHR}.bed.gz"
-	TARGETS=zoo_df['Species'].str.cat(sep=',')
 	THECALL=f'{SLURM_CALL} -s {SOURCE} -t {TARGETS} -o {OUTDIR} -b {ANNOTFN} --snp'
 	print(f'Halper UKB SNPs on {CHR} from {TARGETS}.')
-	os.system(THECALL)
+	# os.system(THECALL)
+
+
+ANNOTFN2=f"{BEDDIR}/UKB_imputed_snps.hg19.unmapped.bed"
+ANNOTFN3=f"{BEDDIR}/UKB_imputed_snps.GRCh38.unmappedlifted.bed"
+if not os.path.isfile(f'{ANNOTFN3}.gz'):
+	df_toLiftOver = notFound.copy()
+	df_toLiftOver['START'] = df_toLiftOver['BP'] - 1
+	df_toLiftOver['SEQ'] = 'chr' + df_toLiftOver['CHR'].astype('str').str.strip('chr')
+	df_toLiftOver.to_csv(ANNOTFN2, columns = ['SEQ','START','BP','NAME'],
+			header = False, index=False, sep = '\t')
+	CHAIN='/home/bnphan/resources/liftOver_chainz/hg19ToHg38.over.chain'
+	THECALL1=f'liftOver {ANNOTFN2} {CHAIN} {ANNOTFN3} {ANNOTFN3}.unmapped'
+	THECALL2=f'gzip {ANNOTFN3}'
+	os.system(THECALL1)
+	os.system(THECALL2)
+
+THECALL3=f'{SLURM_CALL} -s {SOURCE} -t {TARGETS} -o {OUTDIR} -b {ANNOTFN3}.gz --snp'
+os.system(THECALL3)
+
 
 
 

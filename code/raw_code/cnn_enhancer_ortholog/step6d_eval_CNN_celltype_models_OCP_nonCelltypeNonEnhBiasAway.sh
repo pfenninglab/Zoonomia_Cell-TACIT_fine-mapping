26cde863a75017c5edae7241483791998123b583
badoi <badoi@me.com>
2021-04-27 01:41:07 -0400

    run all models on evaluation metircs

--- code/raw_code/cnn_enhancer_ortholog/step6d_eval_CNN_celltype_models_OCP_nonCelltypeNonEnhBiasAway.sh
+++ code/raw_code/cnn_enhancer_ortholog/step6d_eval_CNN_celltype_models_OCP_nonCelltypeNonEnhBiasAway.sh
@@ -2,6 +2,7 @@
 #SBATCH -n 1
 #SBATCH --partition=gpu,pfen3
 #SBATCH --job-name=evalCell
+#SBATCH --time 12:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=93G
 #SBATCH --array=1-40
@@ -23,11 +24,9 @@ CELLS=( NULL MSN_D1 MSN_D2 MSN_SN INT_Pvalb Astro Microglia Oligo OPC )
 CELLTYPE=${CELLS[$CELL_IND]}
 PREFIX=${CELLTYPE}_fold${FOLD}_hgRmMm_${NEGSET}
 
-MODELS=$(ls $DATADIR/models/${CELLTYPE}_fold${FOLD}_hgRmMm_${NEGSET}/*.h5)
-
 #############################################
 ## score species specific evaluation metrics 
-for MODEL in $MODELS; do
+for MODEL in $(ls $DATADIR/models/${CELLTYPE}_fold${FOLD}_hgRmMm*/*.h5); do
 for GENOME in 'hg38' 'mm10' 'rheMac10'; do
 VALIDPOSFILE=$DATADIR/fasta/${GENOME}Only_${CELLTYPE}_fold${FOLD}_valid_positive.fa.gz
 VALIDNEGFILE=$DATADIR/fasta/${GENOME}_${CELLTYPE}_fold${FOLD}_valid_nonEnhNeg.fa.gz

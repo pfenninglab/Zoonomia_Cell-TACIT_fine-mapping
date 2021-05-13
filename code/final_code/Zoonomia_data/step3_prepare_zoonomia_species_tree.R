da4b6aab1d64305aa26cf895fea77c3867e968b6
badoi <badoi@me.com>
2021-05-13 07:11:01 -0400

    group species by MYA divergences from human

--- code/final_code/Zoonomia_data/step3_prepare_zoonomia_species_tree.R
+++ code/final_code/Zoonomia_data/step3_prepare_zoonomia_species_tree.R
@@ -34,6 +34,26 @@ names(col_clade) = levels(df$Clade)
 
 df = df %>% mutate(col_order = col_order[Order], col_clade = col_clade[Clade])
 
+#####################################################################
+# split the species groups up by MYA from human and Order-level LDSC
+df = df %>% mutate(
+  # group the Xenarthra and Afrotheria Orders together by Clade
+  tmp = ifelse(Time.Since.Split.from.Human.TimeTree.median <=94, as.character(Order), as.character(Clade)),
+  col_meta = ifelse(Time.Since.Split.from.Human.TimeTree.median <=94, col_order, col_clade),
+  group_meta = paste(tmp, Time.Since.Split.from.Human.TimeTree.median, sep = '#'), 
+  group_meta = factor(group_meta, unique(group_meta))) %>% select(-tmp)
+df %>% count(group_meta) %>% data.frame()
+
+df_meta = df %>% group_by(group_meta) %>% 
+  select(-c(Zoonomia.Index,Common.Name, Genome.Download.Command_s:Genome.File.Name,
+         starts_with('Time.Since.Split.from.Mouse'))) %>%
+  summarise_all(~ if(is.numeric(.)) {mean(., na.rm = TRUE)} 
+                else paste(unique(.), collapse = ',')) %>%
+  mutate_if(is.character,~ ifelse(.x %in% c('NA', ","), '', .x)) %>%
+  mutate_if(is.character,~ gsub('^NA,', '', .x)) %>%
+  right_join(df %>% count(group_meta, name = 'numSpecies'), by = 'group_meta')
+
+
 ############################
 # read in the species tree #
 tree_fn = file.path(PROJDIR, 'tree','Zoonomia_ChrX_lessGC40_241species_30Consensus.tree')
@@ -42,4 +62,4 @@ tree = read.tree(tree_fn)
 ######################################
 # read in the species in 200m cactus #
 rda_fn = file.path(PROJDIR, 'rdas','200_Mammals_Genome_Information.rda')
-save(df, tree, file = rda_fn)
+save(df,df_meta, tree, file = rda_fn)

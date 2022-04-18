abf69f5c44d8fec1398622f7081edb69a3497ad2
badoi <badoi@me.com>
2022-04-18 00:57:52 -0400

    add the randomized deciles and quintiles

--- code/raw_code/ldsc_celltacit_age_decile/step1_split_CellTACITAge_decile_annotations.R
+++ code/raw_code/ldsc_celltacit_age_decile/step1_split_CellTACITAge_decile_annotations.R
@@ -15,21 +15,11 @@ library(GenomicRanges)
 library(ChIPseeker)
 library(GenomicFeatures)
 
-##############################################
-# 0) human transcriptome annotation via gencode
-sqlite_fn = '/home/bnphan/resources/genomes/GRCh38.p13/gencode.v35.annotation.sqlite'
-if(!file.exists(sqlite_fn)){
-  gff_fn = '/home/bnphan/resources/genomes/GRCh38.p13/gencode.v35.annotation.gff3'
-  txdb = makeTxDbFromGFF(gff_fn)
-  saveDb(txdb, file = sqlite_fn)
-} else {
-  txdb = loadDb(sqlite_fn)
-}
-
 #################################
 # 1) import the Cell Tacit tracks
 DATADIR='data/raw_data/ldsc_celltacit_age_decile'
-bed_fn = list.files('data/raw_data/ldsc_caudate_zoonomia/CellTACIT', full.names = T, pattern = '.bed.gz')
+bed_fn = list.files('data/raw_data/ldsc_caudate_zoonomia/CellTACIT', full.names = T, 
+                    pattern = '.CellTACIT.mean.bed.g')
 names(bed_fn) = basename(bed_fn) %>% ss('Corces2020.|.mean.bed.gz', 2)
 
 #################################
@@ -38,14 +28,18 @@ cell_tacit_peaks = lapply(bed_fn, import) %>%
   lapply(function(gr){
     # break up Ages into deciles per cell type
     gr$decile = ntile(gr$score, 10)
+    gr$decile_rand = ntile(sample(gr$score), 10)
     return(gr)
   })
 
+table_fn = here(DATADIR, 'tables', 'cell_tacit_decile_breaks.xlsx')
 cell_tacit_peaks %>% lapply(as.data.frame) %>%
   rbindlist(idcol = 'celltype') %>% 
   group_by(celltype, decile) %>% 
-  summarise(age_min = min(score),age_max = max(score)) %>% 
-  data.frame()
+  summarise(age_min = min(score) %>% signif(digits = 3),
+            age_max = max(score) %>% signif(digits = 3)) %>% 
+  data.frame() %>% writexl::write_xlsx(table_fn)
+
 
 for (cell in names(cell_tacit_peaks)){
   ## grab the granges of each cell type, split by decile
@@ -55,6 +49,14 @@ for (cell in names(cell_tacit_peaks)){
             paste0('Corces2020.',cell,'.decile',names(tmpList),'.bed.gz'))
   ## export files to bed
   mapply(export, tmpList, fn)
+  
+  ## get the randomized deciles
+  tmpList2 = split(cell_tacit_peaks[[cell]],
+                  cell_tacit_peaks[[cell]]$decile_rand)
+  fn2 = here(DATADIR, 'peaks', 
+            paste0('Corces2020.',cell,'.decile_rand',names(tmpList2),'.bed.gz'))
+  ## export files to bed
+  mapply(export, tmpList2, fn2)
 }
 
 
@@ -65,14 +67,18 @@ cell_tacit_peaks = lapply(bed_fn, import) %>%
   lapply(function(gr){
   # break up Ages into quintile per cell type
   gr$quintile = ntile(gr$score, 5)
+  gr$decile_rand = ntile(sample(gr$score), 5)
   return(gr)
 })
 
+table_fn = here(DATADIR, 'tables', 'cell_tacit_quintile_breaks.xlsx')
 cell_tacit_peaks %>% lapply(as.data.frame) %>%
   rbindlist(idcol = 'celltype') %>% 
   group_by(celltype, quintile) %>% 
-  summarise(age_min = min(score),age_max = max(score)) %>% 
-  data.frame()
+  summarise(age_min = min(score) %>% signif(digits = 3),
+            age_max = max(score) %>% signif(digits = 3)) %>% 
+  data.frame() %>% writexl::write_xlsx(table_fn)
+
 
 DATADIR='data/raw_data/ldsc_celltacit_age_decile'
 dir.create('data/raw_data/ldsc_celltacit_age_decile/peaks', showWarnings = F)
@@ -82,9 +88,17 @@ for (cell in names(cell_tacit_peaks)){
                   cell_tacit_peaks[[cell]]$quintile)
   fn = here(DATADIR, 'peaks', 
             paste0('Corces2020.',cell,'.quintile',names(tmpList),'.bed.gz'))
-  
   ## export files to bed
   mapply(export, tmpList, fn)
+  
+  
+  ## get the randomized quintiles
+  tmpList2 = split(cell_tacit_peaks[[cell]],
+                   cell_tacit_peaks[[cell]]$decile_rand)
+  fn2 = here(DATADIR, 'peaks', 
+             paste0('Corces2020.',cell,'.quintile_rand',names(tmpList2),'.bed.gz'))
+  ## export files to bed
+  mapply(export, tmpList2, fn2)
 }
 
 

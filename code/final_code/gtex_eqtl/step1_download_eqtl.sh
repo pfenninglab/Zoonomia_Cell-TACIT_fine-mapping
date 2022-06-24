6dfa53263069700525e8a19d045eeebca7802120
badoi <badoi@me.com>
2022-06-23 21:33:17 -0400

    download code and preprocess GTEx v8 eQTLs

--- code/final_code/gtex_eqtl/step1_download_eqtl.sh
+++ code/final_code/gtex_eqtl/step1_download_eqtl.sh
@@ -1,10 +1,32 @@
 PROJDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/snATAC_cross_species_caudate
 DATADIR=$PROJDIR/data/tidy_data/gtex_eqtl
 mkdir -p $DATADIR; cd $DATADIR
+
 wget https://storage.googleapis.com/gtex_analysis_v8/single_tissue_qtl_data/GTEx_v8_finemapping_CAVIAR.tar
 wget https://storage.googleapis.com/gtex_analysis_v8/single_tissue_qtl_data/GTEx_Analysis_v8_eQTL.tar
 
 tar xvf GTEx_v8_finemapping_CAVIAR.tar
 rm GTEx_v8_finemapping_CAVIAR.tar
-cd GTEx_v8_finemapping_CAVIAR
 
+## extract only the caudate specific files
+tar xvf GTEx_Analysis_v8_eQTL.tar
+tar -zxvf GTEx_Analysis_v8_eQTL.tar \
+GTEx_Analysis_v8_eQTL/Brain_Caudate_basal_ganglia.v8.egenes.txt.gz
+
+tar -zxvf GTEx_Analysis_v8_eQTL.tar \
+GTEx_Analysis_v8_eQTL/Brain_Caudate_basal_ganglia.v8.signif_variant_gene_pairs.txt.gz
+rm GTEx_Analysis_v8_eQTL.tar
+
+## download the all SNP-gene associations from release v7
+wget https://storage.googleapis.com/gtex_analysis_v7/single_tissue_eqtl_data/GTEx_Analysis_v7_eQTL_all_associations.tar.gz
+
+tar -zxvf GTEx_Analysis_v7_eQTL_all_associations.tar.gz \
+GTEx_Analysis_v7_eQTL_all_associations/Brain_Caudate_basal_ganglia.allpairs.txt.gz
+rm GTEx_Analysis_v7_eQTL_all_associations.tar.gz
+
+
+## download the all SNP-gene associations from release v8
+gsutil -u assign555 -m cp \
+"gs://gtex-resources/GTEx_Analysis_v8_QTLs/GTEx_Analysis_v8_eQTL_all_associations/Brain_Caudate_basal_ganglia.allpairs.txt.gz" \
+"gs://gtex-resources/GTEx_Analysis_v8_QTLs/GTEx_Analysis_v8_eQTL_all_associations/Brain_Caudate_basal_ganglia.v8.allpairs.parquet" \
+${DATADIR}/GTEx_Analysis_v8_eQTL
--- code/final_code/gtex_eqtl/step2_preprocess_eQTLs.R
+++ code/final_code/gtex_eqtl/step2_preprocess_eQTLs.R
@@ -6,14 +6,44 @@ options(stringsAsFactors = F)
 options(repr.plot.width=11, repr.plot.height=8.5)
 suppressMessages(library(tidyverse))
 suppressMessages(library(data.table))
+library(rtracklayer)
+library(arrow)
+library(GenomicRanges)
 library(here)
 
-DATADIR=
+CODEDIR='code/final_code/gtex_eqtl'
+DATADIR='data/tidy_data/gtex_eqtl'
+PLOTDIR='figures/exploratory/gtex_eqtl'
+
+dir.create(here(DATADIR, 'rdas'), showWarnings = F)
 
 alpha = 0.05
-caudate_eQTL = here('data/tidy_data/gtex_eqtl', 'GTEx_Analysis_v8_eQTL', 
-                    'Brain_Caudate_basal_ganglia.v8.egenes.txt.gz') %>%
-  fread() %>% filter(qval <= alpha )
+caudate_eQTL = here(DATADIR, 'GTEx_v8_finemapping_CAVIAR', 
+                    'CAVIAR_Results_v8_GTEx_LD_ALL_NOCUTOFF_with_Allele.txt.gz') %>% fread()
+caudate_eQTL_gr = caudate_eQTL %>% filter(!duplicated(eQTL)) %>% 
+  mutate(tmp = paste0('chr', CHROM, ':', POS)) %>% 
+  dplyr::select(eQTL, tmp) %>% deframe() %>% GRanges()
+  
+caudate_eQTL_gr %>% saveRDS(here(DATADIR, 'rdas', 'GTEx_v8_finemapping_hg38.gr.rds'))
+caudate_eQTL %>% saveRDS(here(DATADIR, 'rdas', 'GTEx_v8_finemapping.df.rds'))
 
-summary(caudate_eQTL$qval)
-unique(caudate_eQTL$gene_name)
+##
+caudate_eQTL = here(DATADIR, 'GTEx_Analysis_v8_eQTL', 
+                    'Brain_Caudate_basal_ganglia.v8.allpairs.parquet') %>% 
+  read_parquet(col_select = c('gene_id','variant_id', 'pval_nominal')) %>% 
+  mutate(FDR = p.adjust(pval_nominal, 'fdr')) %>% 
+  arrange(pval_nominal) %>% filter(!duplicated(variant_id))
+
+table(caudate_eQTL$FDR < alpha)
+
+##
+caudate_eQTL_gr = caudate_eQTL %>% 
+  mutate(chr = ss(variant_id, '_', 1), pos_19 = ss(variant_id, '_', 2), 
+       tmp = paste0(chr, ':', pos_19)) %>% 
+  dplyr::select(variant_id, tmp) %>% deframe() %>% GRanges()
+seqlevelsStyle(caudate_eQTL_gr) <- "UCSC"
+
+caudate_eQTL_gr %>% saveRDS(here(DATADIR, 'rdas', 'Brain_Caudate_basal_ganglia_hg38.v8.gr.rds'))
+caudate_eQTL %>% saveRDS(here(DATADIR, 'rdas', 'Brain_Caudate_basal_ganglia.v8.df.rds'))
+
+
